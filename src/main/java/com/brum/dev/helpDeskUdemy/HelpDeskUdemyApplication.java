78a5f11c88355f23b716ad4a7890f5beee671c18
rafael36832 <caianarafaelcabral@gmail.com>
2024-07-05 23:42:52 -0300

    Base do projeto

---
+++ src/main/java/com/brum/dev/helpDeskUdemy/HelpDeskUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.brum.dev.helpDeskUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brum/dev/helpDeskUdemy/HelpDeskUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brum.dev.helpDeskUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
