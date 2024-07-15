d55406a334a212222563923a1e71f1e100a8d2ef
rafael36832 <caianarafaelcabral@gmail.com>
2024-07-15 13:37:34 -0300

    Adição do Create para Técnicos

--- src/main/java/com/brum/dev/helpDeskUdemy/controllers/TechnicianController.java
+++ src/main/java/com/brum/dev/helpDeskUdemy/controllers/TechnicianController.java
@@ -1,5 +1,6 @@
 package com.brum.dev.helpDeskUdemy.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brum.dev.helpDeskUdemy.domain.dtos.TechnicianDTO;
 import com.brum.dev.helpDeskUdemy.domain.entities.Technician;
@@ -20,19 +24,27 @@ public class TechnicianController {
 
 	@Autowired
 	private TechnicianService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<TechnicianDTO> findByid(@PathVariable Integer id){
+	public ResponseEntity<TechnicianDTO> findByid(@PathVariable Integer id) {
 		Technician response = this.service.findByid(id);
 		TechnicianDTO dto = new TechnicianDTO(response);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<TechnicianDTO>> findAll(){
+	public ResponseEntity<List<TechnicianDTO>> findAll() {
 		List<Technician> response = this.service.findAll();
-		List<TechnicianDTO> responseDTO = response.stream().map(x -> new TechnicianDTO(x)).collect(Collectors.toList()); 
+		List<TechnicianDTO> responseDTO = response.stream().map(x -> new TechnicianDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(responseDTO);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<TechnicianDTO> create(@RequestBody TechnicianDTO dto) {
+		Technician technician = service.create(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(technician.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/com/brum/dev/helpDeskUdemy/services/TechnicianService.java
+++ src/main/java/com/brum/dev/helpDeskUdemy/services/TechnicianService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.brum.dev.helpDeskUdemy.domain.dtos.TechnicianDTO;
+import com.brum.dev.helpDeskUdemy.domain.entities.Person;
 import com.brum.dev.helpDeskUdemy.domain.entities.Technician;
+import com.brum.dev.helpDeskUdemy.exceptions.DataIntegrityViolationException;
 import com.brum.dev.helpDeskUdemy.exceptions.NotFoundException;
+import com.brum.dev.helpDeskUdemy.repositories.PersonRepository;
 import com.brum.dev.helpDeskUdemy.repositories.TechnicianRepository;
 
 @Service
@@ -16,6 +20,9 @@ public class TechnicianService {
 	@Autowired
 	private TechnicianRepository repository;
 	
+	@Autowired
+	private PersonRepository personRepository;
+	
 	public Technician findByid(Integer id) {
 		Optional<Technician> response = repository.findById(id);
 		return response.orElseThrow(() -> new NotFoundException("Object not found: Technician id: " + id)); 
@@ -23,6 +30,25 @@ public class TechnicianService {
 
 	public List<Technician> findAll() {
 		return repository.findAll();
+	}
+	
+	public Technician create(TechnicianDTO dto) {
+		dto.setId(null);
+		this.verifyCpfAndEmail(dto);
+		Technician technician = new Technician(dto);
+		return repository.save(technician);
+	}
+
+	private void verifyCpfAndEmail(TechnicianDTO dto) {
+		Optional<Person> personByCpf = this.personRepository.findByCpf(dto.getCpf());
+		if(personByCpf.isPresent() && dto.getId() != personByCpf.get().getId()) {
+			throw new DataIntegrityViolationException("CPF already registered");
+		}
+		
+		Optional<Person> personByEmail = this.personRepository.findByEmail(dto.getEmail());
+		if(personByEmail.isPresent() && dto.getId() != personByEmail.get().getId()) {
+			throw new DataIntegrityViolationException("Email already registered");
+		}
 	}
 	
 	
