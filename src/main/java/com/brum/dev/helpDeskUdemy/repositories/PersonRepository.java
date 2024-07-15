f95605c0503b637bb5d2ecbe93a4d0ac9fa0415b
rafael36832 <caianarafaelcabral@gmail.com>
2024-07-15 13:40:10 -0300

    getByEmail e getByCPF em Person

--- src/main/java/com/brum/dev/helpDeskUdemy/repositories/PersonRepository.java
+++ src/main/java/com/brum/dev/helpDeskUdemy/repositories/PersonRepository.java
@@ -1,9 +1,15 @@
 package com.brum.dev.helpDeskUdemy.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brum.dev.helpDeskUdemy.domain.entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Integer>{
 
+	Optional<Person> findByEmail(String email);
+	
+	Optional<Person> findByCpf(String cpf);
+	
 }
